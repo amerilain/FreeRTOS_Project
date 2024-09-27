abbea3d68b38709f8db9dbf5a8808973a154830b
abhinav-kayastha <Abhinav.Kayastha@metropolia.fi>
2024-09-27 15:55:45 +0300

    reverting back to old main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <sstream>
-#include <cstring>
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
 #include "hardware/gpio.h"
-#include "IPStack.h"
+#include "PicoOsUart.h"
+#include "ssd1306.h"
+
 
 #include "hardware/timer.h"
 extern "C" {
@@ -14,127 +15,243 @@ uint32_t read_runtime_ctr(void) {
 }
 }
 
-// abhinav was here
+#include "blinker.h"
 
-#if 1
-#define HTTP_SERVER        "3.224.58.169"
-//#define HTTP_SERVER        "api.thingspeak.com"
-#define BUFSIZE 2048
-#endif
+SemaphoreHandle_t gpio_sem;
 
-/* Add a command to Talkback queue with curl (a command line utility)
- * curl -v -d "command_string=my%20fancy%20command&api_key=371DAWENQKI6J8DD" http://api.thingspeak.com/talkbacks/52920/commands
- * */
-void test_task(void *param) {
-    (void) param;
-#if 0
-    // this works
-    const char *req = "POST /talkbacks/52920/commands/execute.json HTTP/1.1\r\n"
-                      "Host: api.thingspeak.com\r\n"
-                      "User-Agent: PicoW\r\n"
-                      "Accept: */*\r\n"
-                      "Content-Length: 24\r\n"
-                      "Content-Type: application/x-www-form-urlencoded\r\n"
-                      "\r\n"
-                      "api_key=371DAWENQKI6J8DD";
-#endif
-#if 1
-    // Execute (= get and remove) next command from talkback queue - tested to work
-    const char *req = "POST /talkbacks/52920/commands/execute.json HTTP/1.1\r\n"
-                      "Host: api.thingspeak.com\r\n"
-                      "Content-Length: 24\r\n"
-                      "Content-Type: application/x-www-form-urlencoded\r\n"
-                      "\r\n"
-                      "api_key=371DAWENQKI6J8DD";
-#endif
-#if 0
-    // Update fields using a POST request and execute (= get and remove) next command from talkback queue - tested to work
-    const char *req = "POST /update.json HTTP/1.1\r\n"
-                      "Host: api.thingspeak.com\r\n"
-                      "User-Agent: PicoW\r\n"
-                      "Accept: */*\r\n"
-                      "Content-Length: 65\r\n"
-                      "Content-Type: application/x-www-form-urlencoded\r\n"
-                      "\r\n"
-                      "field1=370&api_key=1WWH2NWXSM53URR5&talkback_key=371DAWENQKI6J8DD";
-#endif
-#if 0
-    // Update fields using a GET request - tested to work
-    const char *req = "GET /update?api_key=1WWH2NWXSM53URR5&field1=440&field2=44.7 HTTP/1.1\r\n"
-                      "Host: api.thingspeak.com\r\n"
-                      "User-Agent: PicoW\r\n"
-                      "Accept: */*\r\n"
-                      "Content-Length: 0\r\n"
-                      "Content-Type: application/x-www-form-urlencoded\r\n"
-                      "\r\n";
-#endif
-#if 0
-    // Update fields using a minimal GET request - tested to work
-    const char *req = "GET /update?api_key=1WWH2NWXSM53URR5&field1=410&field2=45.7 HTTP/1.1\r\n"
-                      "Host: api.thingspeak.com\r\n"
-                      "\r\n";
-#endif
-#if 0
-    // List all talkback commands - tested to work
-    const char *req = "GET /talkbacks/52920/commands.json?api_key=371DAWENQKI6J8DD HTTP/1.1\r\n"
-                      "Host: api.thingspeak.com\r\n"
-                      "\r\n";
-#endif
-    printf("\nconnecting...\n");
+void gpio_callback(uint gpio, uint32_t events) {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    // signal task that a button was pressed
+    xSemaphoreGiveFromISR(gpio_sem, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
 
-    unsigned char *buffer = new unsigned char[BUFSIZE];
-    // todo: Add failed connection handling
-    //IPStack ipstack("SmartIotMQTT", "SmartIot"); // example
-    IPStack ipstack("Nadim", "nadimahmed"); // Set env in CLion CMAKE setting
+struct led_params{
+    uint pin;
+    uint delay;
+};
 
-    const uint led_pin = 22;
-    // Initialize LED pin
+void blink_task(void *param)
+{
+    auto lpr = (led_params *) param;
+    const uint led_pin = lpr->pin;
+    const uint delay = pdMS_TO_TICKS(lpr->delay);
     gpio_init(led_pin);
     gpio_set_dir(led_pin, GPIO_OUT);
+    while (true) {
+        gpio_put(led_pin, true);
+        vTaskDelay(delay);
+        gpio_put(led_pin, false);
+        vTaskDelay(delay);
+    }
+}
 
-    const uint button = 9;
-    gpio_init(button);
-    gpio_set_dir(button, GPIO_IN);
-    gpio_pull_up(button);
-
+void gpio_task(void *param) {
+    (void) param;
+    const uint button_pin = 9;
+    const uint led_pin = 22;
+    const uint delay = pdMS_TO_TICKS(250);
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+    gpio_init(button_pin);
+    gpio_set_dir(button_pin, GPIO_IN);
+    gpio_set_pulls(button_pin, true, false);
+    gpio_set_irq_enabled_with_callback(button_pin, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
     while(true) {
-        //std::cout << "button event\n";
-        gpio_put(led_pin, 1);
-        vTaskDelay(300);
-        gpio_put(led_pin, 0);
-        vTaskDelay(300);
-        if(gpio_get(button) == 0) {
-            int rc = ipstack.connect(HTTP_SERVER, 80);
-            if (rc == 0) {
-                ipstack.write((unsigned char *) (req), strlen(req), 1000);
-                auto rv = ipstack.read(buffer, BUFSIZE, 2000);
-                buffer[rv] = 0;
-                printf("rv=%d\n%s\n", rv, buffer);
-                ipstack.disconnect();
-            }
-            else {
-                printf("rc from TCP connect is %d\n", rc);
+        if(xSemaphoreTake(gpio_sem, portMAX_DELAY) == pdTRUE) {
+            //std::cout << "button event\n";
+            gpio_put(led_pin, 1);
+            vTaskDelay(delay);
+            gpio_put(led_pin, 0);
+            vTaskDelay(delay);
+        }
+    }
+}
+
+void serial_task(void *param)
+{
+    PicoOsUart u(0, 0, 1, 115200);
+    Blinker blinky(20);
+    uint8_t buffer[64];
+    std::string line;
+    while (true) {
+        if(int count = u.read(buffer, 63, 30); count > 0) {
+            u.write(buffer, count);
+            buffer[count] = '\0';
+            line += reinterpret_cast<const char *>(buffer);
+            if(line.find_first_of("\n\r") != std::string::npos){
+                u.send("\n");
+                std::istringstream input(line);
+                std::string cmd;
+                input >> cmd;
+                if(cmd == "delay") {
+                    uint32_t i = 0;
+                    input >> i;
+                    blinky.on(i);
+                }
+                else if (cmd == "off") {
+                    blinky.off();
+                }
+                line.clear();
             }
         }
     }
 }
 
-
+void modbus_task(void *param);
+void display_task(void *param);
+void i2c_task(void *param);
+extern "C" {
+    void tls_test(void);
+}
+void tls_task(void *param)
+{
+    tls_test();
+    while(true) {
+        vTaskDelay(100);
+    }
+}
 
 int main()
 {
+    static led_params lp1 = { .pin = 20, .delay = 300 };
     stdio_init_all();
+    printf("\nBoot\n");
 
+    gpio_sem = xSemaphoreCreateBinary();
     //xTaskCreate(blink_task, "LED_1", 256, (void *) &lp1, tskIDLE_PRIORITY + 1, nullptr);
     //xTaskCreate(gpio_task, "BUTTON", 256, (void *) nullptr, tskIDLE_PRIORITY + 1, nullptr);
     //xTaskCreate(serial_task, "UART1", 256, (void *) nullptr,
     //            tskIDLE_PRIORITY + 1, nullptr);
-
-    xTaskCreate(test_task, "http", 1024, (void *) nullptr,
+#if 0
+    xTaskCreate(modbus_task, "Modbus", 512, (void *) nullptr,
                 tskIDLE_PRIORITY + 1, nullptr);
 
+
+    xTaskCreate(display_task, "SSD1306", 512, (void *) nullptr,
+                tskIDLE_PRIORITY + 1, nullptr);
+
+    xTaskCreate(i2c_task, "i2c test", 512, (void *) nullptr,
+                tskIDLE_PRIORITY + 1, nullptr);
+#endif
+    xTaskCreate(tls_task, "tls test", 6000, (void *) nullptr,
+                tskIDLE_PRIORITY + 1, nullptr);
 
     vTaskStartScheduler();
 
     while(true){};
+}
+
+#include <cstdio>
+#include "ModbusClient.h"
+#include "ModbusRegister.h"
+
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#if 0
+#define UART_NR 0
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+#else
+#define UART_NR 1
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+#endif
+
+#define BAUD_RATE 9600
+#define STOP_BITS 2 // for real system (pico simualtor also requires 2 stop bits)
+
+#define USE_MODBUS
+
+void modbus_task(void *param) {
+
+    const uint led_pin = 22;
+    const uint button = 9;
+
+    // Initialize LED pin
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    gpio_init(button);
+    gpio_set_dir(button, GPIO_IN);
+    gpio_pull_up(button);
+
+    // Initialize chosen serial port
+    //stdio_init_all();
+
+    //printf("\nBoot\n");
+
+#ifdef USE_MODBUS
+    auto uart{std::make_shared<PicoOsUart>(UART_NR, UART_TX_PIN, UART_RX_PIN, BAUD_RATE, STOP_BITS)};
+    auto rtu_client{std::make_shared<ModbusClient>(uart)};
+    ModbusRegister rh(rtu_client, 241, 256);
+    ModbusRegister t(rtu_client, 241, 257);
+    ModbusRegister produal(rtu_client, 1, 0);
+    produal.write(100);
+    vTaskDelay((100));
+    produal.write(100);
+#endif
+
+    while (true) {
+#ifdef USE_MODBUS
+        gpio_put(led_pin, !gpio_get(led_pin)); // toggle  led
+        printf("RH=%5.1f%%\n", rh.read() / 10.0);
+        vTaskDelay(5);
+        printf("T =%5.1f%%\n", t.read() / 10.0);
+        vTaskDelay(3000);
+#endif
+    }
+
+
+}
+
+#include "ssd1306os.h"
+void display_task(void *param)
+{
+    auto i2cbus{std::make_shared<PicoI2C>(1, 400000)};
+    ssd1306os display(i2cbus);
+    display.fill(0);
+    display.text("Boot", 0, 0);
+    display.show();
+    while(true) {
+        vTaskDelay(100);
+    }
+
+}
+
+void i2c_task(void *param) {
+    auto i2cbus{std::make_shared<PicoI2C>(0, 100000)};
+
+    const uint led_pin = 21;
+    const uint delay = pdMS_TO_TICKS(250);
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    uint8_t buffer[64] = {0};
+    i2cbus->write(0x50, buffer, 2);
+
+    auto rv = i2cbus->read(0x50, buffer, 64);
+    printf("rv=%u\n", rv);
+    for(int i = 0; i < 64; ++i) {
+        printf("%c", isprint(buffer[i]) ? buffer[i] : '_');
+    }
+    printf("\n");
+
+    buffer[0]=0;
+    buffer[1]=64;
+    rv = i2cbus->transaction(0x50, buffer, 2, buffer, 64);
+    printf("rv=%u\n", rv);
+    for(int i = 0; i < 64; ++i) {
+        printf("%c", isprint(buffer[i]) ? buffer[i] : '_');
+    }
+    printf("\n");
+
+    while(true) {
+        gpio_put(led_pin, 1);
+        vTaskDelay(delay);
+        gpio_put(led_pin, 0);
+        vTaskDelay(delay);
+    }
+
+
 }
